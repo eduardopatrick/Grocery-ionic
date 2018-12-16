17185929070bacc47f73c0c8fb0162daa7c6b82e
Eduardo Patrick <eduardo.patrick.98@gmail.com>
2018-12-16 02:52:52 -0300

    First Commit - Starting changes on project

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule, LOCALE_ID  } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { SQLite } from '@ionic-native/sqlite'
+import { DatabaseProvider } from '../providers/database/database';
+import { ProductProvider } from '../providers/products/products';
+import { CategoryProvider } from '../providers/category/category';
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    // Grande sacada para formatar numeros e datas no formato brasileiro
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    SQLite,
+    DatabaseProvider,
+    ProductProvider,
+    CategoryProvider,
+    DatabaseProvider,
+    CategoryProvider
+  ]
+})
+export class AppModule {}
